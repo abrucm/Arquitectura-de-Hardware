4bce9385a65bdcfe5b4e9ec847c1482282771b34
abrucm <83360842+abrucm@users.noreply.github.com>
2021-05-18 08:59:33 -0300

    Update sketch4_may13a.ino

--- sketch4_may13a.ino
+++ sketch4_may13a.ino
@@ -12,3 +12,22 @@ void loop()
   Serial.println(variable);
   delay(20);
 }
+
+//OTRA FORMA:
+
+//int variable = 0;
+
+//void setup()
+//{
+//  Serial.begin(9600);
+//}
+
+//void loop()
+//{
+//  for(int i =0;i<360; i++)
+    
+//  {
+//    Serial.println(i);
+//    delay(20);
+//  }
+//}
